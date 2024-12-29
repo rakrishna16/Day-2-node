62c484a3ea4908a880d4b44d4efa03e2399da285
rakrishna16 <rakrishna16@gmail.com>
2024-12-29 16:46:52 +0530

    CRUD operation

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -1,7 +1,7 @@
 const products = [
-  { id: 1, name: "iphone12", price: 25000 },
-  { id: 2, name: "laptop", price: 55000 },
-  { id: 3, name: "headphone", price: 5000 },
+  { id: 1, name: "iphone16", price: 25000 },
+  { id: 2, name: "Dell", price: 55000 },
+  { id: 3, name: "Samsung", price: 5000 },
   { id: 4, name: "printer", price: 15000 },
 ];
 
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# CRUD operation using Node js, Express js
+
+# CRUD operation documentsion link mentioned below
+
+# https://documenter.getpostman.com/view/31006727/2sAYJ6DL33
